6cf87f484e9dbed77436ea436d28b9ef49763f8f
nkd0310 <nkd0310@naver.com>
2021-02-22 16:40:05 +0900

    JavaScript 문법 - 배열

---
+++ syntax/array.js
@@ -0,0 +1,8 @@
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = '3';
+console.log(arr);
+console.log(arr.length);  //배열 길이
+arr.push('E');  //배열 끝에 삽입
+console.log(arr);
