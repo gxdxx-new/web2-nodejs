ee388f5893023bbf6c43c4dc3edcb90a593e3045
nkd0310 <nkd0310@naver.com>
2021-02-22 17:06:54 +0900

    Node.js에서 파일목록 알아내기

---
+++ nodejs/readdir.js
@@ -0,0 +1,14 @@
+const testFolder = './data';
+const fs = require('fs');
+
+fs.readdir(testFolder, function(error, filelist) {
+  console.log(filelist);
+})
+
+/*
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});
+*/
