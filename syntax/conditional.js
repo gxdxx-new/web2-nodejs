509bfd9ffa9dd415c363c76a81e427ee926d9457
nkd0310 <nkd0310@naver.com>
2021-02-22 10:18:37 +0900

    JavaScript 문법 - 콘솔에서의 입력값

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,6 +1,8 @@
+var args = process.argv; //배열 형식 출력
+console.log(args[2]); //입력한 입력값은 3번째 자리부터 시작
 console.log('A');
 console.log('B');
-if(true) {
+if(args[2] === '1') {
   console.log('C1');
 } else {
   console.log('C2');
