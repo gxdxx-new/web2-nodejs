62d55cee9b11c8809a70b409542bf36f0e37c6e0
nkd0310 <nkd0310@naver.com>
2021-02-23 12:12:03 +0900

    main.js 주석 추가

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/') {
-      if(queryData.id === undefined) {
+      if(queryData.id === undefined) {  //id값이 없는 경우
+        //파일 목록을 가져옴. filelist에는 data디렉토리의 값들이 들어옴
         fs.readdir('./data', function(error, filelist) {
           var title = 'Welcome';
           var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length) {
+            //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
+          list += '</ul>';
           /*
           var list = `<ul>
             <li><a href="/?id=HTML">HTML</a></li>
@@ -18,13 +27,6 @@ var app = http.createServer(function(request,response){
             <li><a href="/?id=JavaScript">JavaScript</a></li>
           </ul>`;
           */
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
           var template = `
           <!doctype html>
           <html>
@@ -41,12 +43,18 @@ var app = http.createServer(function(request,response){
           </html>
           `;
           response.writeHead(200);  //파일을 성공적으로 전송
-          response.end(template);
+          response.end(template); //template을 보여줌
         });
-      } else {
+      } else {  //id값이 있는 경우
         fs.readdir('./data', function(error, filelist) {
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length) {
+            //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
+          list += '</ul>';
           /*
           var list = `<ul>
             <li><a href="/?id=HTML">HTML</a></li>
@@ -54,13 +62,6 @@ var app = http.createServer(function(request,response){
             <li><a href="/?id=JavaScript">JavaScript</a></li>
           </ul>`;
           */
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
             var title = queryData.id;
             var template = `
@@ -78,15 +79,15 @@ var app = http.createServer(function(request,response){
             </body>
             </html>
             `;
-            response.writeHead(200);
-            response.end(template);
+            response.writeHead(200);  //파일을 성공적으로 전송
+            response.end(template); //template을 보여줌
           });
         });
       }
     } else {
         response.writeHead(400);  //파일을 찾을 수 없음
-        response.end('Not found');
+        response.end('Not found');  //Not found을 보여줌
       }
   });
 
-app.listen(3000);
+app.listen(3000); //localhost:3000
