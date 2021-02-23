dfe323b63bd88e959a0fe21539642c9fa5f5f9f5
nkd0310 <nkd0310@naver.com>
2021-02-23 22:09:51 +0900

    App 제작 - 함수를 이용해서 정리

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 var http = require('http');
-var fs = require('fs');
+var fs = require('fs'); //node.js의 모듈인 fileSystem을 다룰 수 있게됨
 var url = require('url'); //모듈
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filslist) {
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length) {
+    //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list += '</ul>';
+  return list;
+  /*
+  var list = `<ul>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ul>`;
+  */
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -8,77 +44,23 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/') {
       if(queryData.id === undefined) {  //id값이 없는 경우
-        //파일 목록을 가져옴. filelist에는 data디렉토리의 값들이 들어옴
+        //`./data`디렉토리에 있는 파일 목록을 가져옴. filelist에는 data디렉토리의 파일명들이 들어옴
         fs.readdir('./data', function(error, filelist) {
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list += '</ul>';
-          /*
-          var list = `<ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>`;
-          */
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);  //파일을 성공적으로 전송
           response.end(template); //template을 보여줌
         });
       } else {  //id값이 있는 경우
+        //`./data`디렉토리에 있는 파일 목록을 가져옴. filelist에는 data디렉토리의 파일명들이 들어옴
         fs.readdir('./data', function(error, filelist) {
-          var list = '<ul>';
-          var i = 0;
-          while(i < filelist.length) {
-            //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i++;
-          }
-          list += '</ul>';
-          /*
-          var list = `<ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>`;
-          */
+          //`data/${queryData.id}` 파일의 내용을 읽어서 description변수에 저장
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-            `;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);  //파일을 성공적으로 전송
             response.end(template); //template을 보여줌
           });
