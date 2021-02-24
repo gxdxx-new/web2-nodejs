58424438189c75c913fd7378207486876a69f014
nkd0310 <nkd0310@naver.com>
2021-02-24 23:35:15 +0900

    App 제작 - 글수정_수정링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs'); //node.js의 모듈인 fileSystem을 다룰 수 있게�
 var url = require('url'); //모듈
 var qs = require('querystring');
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
   <body>  <!--페이지 내용-->
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -52,7 +52,10 @@ var app = http.createServer(function(request,response){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>` //home에서는 update 버튼 안나오게
+          );
           response.writeHead(200);  //파일을 성공적으로 전송
           response.end(template); //template을 보여줌
         });
@@ -63,7 +66,9 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
             var title = queryData.id;
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
             response.writeHead(200);  //파일을 성공적으로 전송
             response.end(template); //template을 보여줌
           });
@@ -84,7 +89,7 @@ var app = http.createServer(function(request,response){
               <input type="submit"> <!--전송버튼-->
             </p>
           </form>
-          `);
+          `, '');
         response.writeHead(200);  //파일을 성공적으로 전송
         response.end(template); //template을 보여줌
       });
