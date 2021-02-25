602cf7b9bdfe5b01152173779d7f9abf5f82e42f
nkd0310 <nkd0310@naver.com>
2021-02-25 13:50:03 +0900

    App 제작 - 글수정_파일명 변경, 내용 저장

--- main.js
+++ main.js
@@ -22,14 +22,14 @@ function templateHTML(title, list, body, control) {
 }
 
 function templateList(filelist) {
-  var list = '<ul>';
+  var list = `<ul>`;
   var i = 0;
   while(i < filelist.length) {
     //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
     list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i++;
   }
-  list += '</ul>';
+  list += `</ul>`;
   return list;
   /*
   var list = `<ul>
@@ -41,7 +41,7 @@ function templateList(filelist) {
 }
 //createServer은 Nodejs로 웹브라우저가 접속이 들어올 때마다 callback함수를 Nodejs가 호출
 //request(요청할 때 웹브라우저가 보낸 정보들), response(응답할 때 우리가 웹브라우저에게 전송할 정보들)
-var app = http.createServer(function(request,response){
+var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
@@ -115,8 +115,7 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list,
-            `
+          var template = templateHTML(title, list, `
             <form action="/update_process" method="post">  <!--form 아래 입력한 정보를 주소로 전송-->
               <input type ="hidden" name="id" value="${title}">  <!--id값은 변경되지않음.-->
               <p><input type="text" name="title" value="${title}"></p>  <!--한줄 입력, value="${title}가 title에 기본값으로 들어오게 함"-->
@@ -131,6 +130,24 @@ var app = http.createServer(function(request,response){
             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);  //파일을 성공적으로 전송
           response.end(template); //template을 보여줌
+        });
+      });
+    } else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data) { //data를 한개씩 받다가 마지막에 'end'다음의 callback함수를 호출
+        body += data; //웹브라우저가 보낸 정보들을 저장
+      });
+      request.on('end', function() {
+        var post = qs.parse(body);  //post변수에 post정보를 저장(querystring)
+        var id = post.id; //수정할 때는 id값이 필요
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) { //fs.rename(oldPath, newPath, callback)
+          //파일을 저장 (`data/${title}`은 생성할 파일, description은 저장할 내용)
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err) {  //err:에러가 있을 경우 에러를 처리하는 방법을 제공
+            response.writeHead(302, {Location: `/?id=${title}`});  //리다이렉션(Location으로 이동)
+            response.end();
+          });
         });
       });
     } else {
