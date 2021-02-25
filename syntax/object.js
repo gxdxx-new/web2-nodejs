59efd4f47ca069e180d781de33db966f82723b9d
nkd0310 <nkd0310@naver.com>
2021-02-25 19:49:41 +0900

    JavaScript 문법 - 객체의 형식

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['gidon', '5356', 'yk'];
+console.log(members[1]);  //5356
+
+//객체 : 각각의 데이터마다 고유한 이름을 부여
+var roles = {
+  'programmer':'gidon',
+  'designer':'5356',
+  'manager':'yk'
+};
+console.log(roles.designer);
