73cac202e26f03ef778a48133b3dbb83b0da151c
nkd0310 <nkd0310@naver.com>
2021-02-26 15:11:13 +0900

    JavaScript 문법 - 객체_반복

--- syntax/object.js
+++ syntax/object.js
@@ -1,6 +1,12 @@
 var members = ['gidon', '5356', 'yk'];
 console.log(members[1]);  //5356
 
+var i = 0;
+while(i < members.length) {
+  console.log('array loop =>', members[i]);
+  i++;
+}
+
 //객체 : 각각의 데이터마다 고유한 이름을 부여
 var roles = {
   'programmer':'gidon',
@@ -8,3 +14,8 @@ var roles = {
   'manager':'yk'
 };
 console.log(roles.designer);
+console.log(roles['designer']); //둘다 사용가능
+
+for(var name in roles) {
+  console.log('object =>', name, '/ value =>', roles[name]);
+}
