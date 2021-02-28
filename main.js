846f92a2cf6766b38da79a7dd4b888595da175a8
nkd0310 <nkd0310@naver.com>
2021-02-28 20:06:31 +0900

    App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -2,44 +2,7 @@ var http = require('http');
 var fs = require('fs'); //node.js의 모듈인 fileSystem을 다룰 수 있게됨
 var url = require('url'); //모듈
 var qs = require('querystring');
-
-var template = {
-  HTML:function(title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>  <!--페이지 제목-->
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>  <!--페이지 내용-->
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list:function(filelist) {
-    var list = `<ul>`;
-    var i = 0;
-    while(i < filelist.length) {
-      //앞 filelist[i]는 링크를 위해, 뒤 filslist[i]는 보여지는 값
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i++;
-    }
-    list += `</ul>`;
-    return list;
-    /*
-    var list = `<ul>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>`;
-    */
-  }
-};
+var template = require('./lib/template.js');
 
 //createServer은 Nodejs로 웹브라우저가 접속이 들어올 때마다 callback함수를 Nodejs가 호출
 //request(요청할 때 웹브라우저가 보낸 정보들), response(응답할 때 우리가 웹브라우저에게 전송할 정보들)
