475cd769614fc3b28d881ba809e628d46d668827
gxdxx <nkd0310@naver.com>
2021-03-02 23:57:31 +0900

    MySQL 모듈 사용

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql      = require('mysql');  //mysql 모듈을 사용
+var connection = mysql.createConnection({   //mysql 모듈과 관련된 객체의 createConnection메소드를 호출
+  host     : 'localhost',
+  user     : 'root',
+  password : '000000',
+  database : 'opentutorials'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM topic', function (error, results, fields) {    //sql문, callback을 인자로 줌
+  if (error) {
+      console.log(error);
+  }
+  console.log(results); //접속결과가 담긴 results인자를 출력
+});
+ 
+connection.end();
