3dc30c4abf083357a489d87b51c3e8848922145e
gxdxx <nkd0310@naver.com>
2021-03-03 10:19:35 +0900

    ER ACCESS DENIED ERROR 오류 처리

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,8 +1,9 @@
 var mysql      = require('mysql');  //mysql 모듈을 사용
 var connection = mysql.createConnection({   //mysql 모듈과 관련된 객체의 createConnection메소드를 호출
   host     : 'localhost',
-  user     : 'root',
+  user     : 'nodejs',
   password : '000000',
+  port     : '3307',
   database : 'opentutorials'
 });
  
