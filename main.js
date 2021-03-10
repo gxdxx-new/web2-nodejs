f5e90d85538fa549a3feb608b53d6e9d3ffa08a6
gxdxx <nkd0310@naver.com>
2021-03-10 17:37:37 +0900

    Express - Hello World 2

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) { //routing
+  return res.send('/')
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/page', function(req, res) { //routing
+  return res.send('/page')
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 /*
 var http = require('http');
