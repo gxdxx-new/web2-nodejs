39f46b59697cf59ea4f0654f8339a80db10b1189
gxdxx <nkd0310@naver.com>
2021-03-12 23:51:39 +0900

    Express - 에러 처리

--- main.js
+++ main.js
@@ -22,9 +22,22 @@ app.get('/', function(request, response) { //routing
   topic.home(request, response);
 });
 
-app.get('/page/:pageId', function(request, response) { //routing
-  topic.page(request, response);
-  //response.send(request.params);  //request.params => :pageId에 들어있는 값
+app.get('/page/:pageId', function(request, response, next) { //routing
+  db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [request.params.pageId], function(error, topic1) { //보안을 위해 sql문에 ?로 두번째 인자가 치환되도록 함(?은 문자가 돼서 DROP문을 입력해도 문자로 처리해서 공격을 막을 수 있음)
+    if(error) {
+        next(error);
+    } else{
+      try {
+      topic.page(request, response, topic1);
+      //response.send(request.params);  //request.params => :pageId에 들어있는 값
+      } catch(error) {
+        if(error) {
+          console.log('error occuered!')
+          next(error);
+        }
+      }
+    }
+  });
 });
 
 app.get('/create', function(request, response) {
@@ -47,51 +60,59 @@ app.post('/delete_process', function(request, response) {
   topic.delete_process(request, response);
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-/*
-var http = require('http');
-var url = require('url'); //모듈
-var topic = require('./lib/topic.js');
-var author = require('./lib/author.js');
-
-//createServer은 Nodejs로 웹브라우저가 접속이 들어올 때마다 callback함수를 Nodejs가 호출
-//request(요청할 때 웹브라우저가 보낸 정보들), response(응답할 때 우리가 웹브라우저에게 전송할 정보들)
-var app = http.createServer(function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/') {
-      if(queryData.id === undefined) {  //id값이 없는 경우(홈 화면)
-        topic.home(request, response); //request, response를 전달
-      } else {  //id값이 있는 경우
-        topic.page(request, response);
-      }
-    } else if(pathname === '/create') { //create버튼을 클릭하면 입력 상자가 생김
-      topic.create(request, response);
-    } else if(pathname === '/create_process') { //입력상자에 입력을 다 하고 create버튼을 클릭하면 /create_process로 이동
-      topic.create_process(request, response);
-    } else if(pathname === '/update') {
-      topic.update(request, response);
-    } else if(pathname === '/update_process') {
-      topic.update_process(request, response);
-    } else if(pathname === '/delete_process') {
-      topic.delete_process(request, response);
-    } else if(pathname === '/author') {
-      author.home(request, response);
-    } else if(pathname === '/author/create_process') {
-      author.create_process(request, response);
-    } else if(pathname === '/author/update') {
-      author.update(request, response);
-    } else if(pathname === '/author/update_process') {
-      author.update_process(request, response);
-    } else if(pathname === '/author/delete_process') {
-      author.delete_process(request, response);
-    } else {
-        response.writeHead(404);  //파일을 찾을 수 없음
-        response.end('Not found');  //Not found을 보여줌
-      }
+app.use(function(request, response, next) { //미들웨어는 순차적으로 실행되기 때문에 위에서 실행이 안되고 여기까지 오게되면 못찾은거여서 에러처리
+  response.status(404).send('Sorry cant find that!');
 });
 
-app.listen(3000); //localhost:3000
-*/
+app.use(function(error, request, response, next) {
+  console.error(error.stack);
+  response.status(500).send('Something broke!');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+
+// var http = require('http');
+// var url = require('url'); //모듈
+// var topic = require('./lib/topic.js');
+// var author = require('./lib/author.js');
+
+// //createServer은 Nodejs로 웹브라우저가 접속이 들어올 때마다 callback함수를 Nodejs가 호출
+// //request(요청할 때 웹브라우저가 보낸 정보들), response(응답할 때 우리가 웹브라우저에게 전송할 정보들)
+// var app = http.createServer(function(request, response) {
+//     var _url = request.url;
+//     var queryData = url.parse(_url, true).query;
+//     var pathname = url.parse(_url, true).pathname;
+//     if(pathname === '/') {
+//       if(queryData.id === undefined) {  //id값이 없는 경우(홈 화면)
+//         topic.home(request, response); //request, response를 전달
+//       } else {  //id값이 있는 경우
+//         topic.page(request, response);
+//       }
+//     } else if(pathname === '/create') { //create버튼을 클릭하면 입력 상자가 생김
+//       topic.create(request, response);
+//     } else if(pathname === '/create_process') { //입력상자에 입력을 다 하고 create버튼을 클릭하면 /create_process로 이동
+//       topic.create_process(request, response);
+//     } else if(pathname === '/update') {
+//       topic.update(request, response);
+//     } else if(pathname === '/update_process') {
+//       topic.update_process(request, response);
+//     } else if(pathname === '/delete_process') {
+//       topic.delete_process(request, response);
+//     } else if(pathname === '/author') {
+//       author.home(request, response);
+//     } else if(pathname === '/author/create_process') {
+//       author.create_process(request, response);
+//     } else if(pathname === '/author/update') {
+//       author.update(request, response);
+//     } else if(pathname === '/author/update_process') {
+//       author.update_process(request, response);
+//     } else if(pathname === '/author/delete_process') {
+//       author.delete_process(request, response);
+//     } else {
+//         response.writeHead(404);  //파일을 찾을 수 없음
+//         response.end('Not found');  //Not found을 보여줌
+//       }
+// });
+
+// app.listen(3000);
