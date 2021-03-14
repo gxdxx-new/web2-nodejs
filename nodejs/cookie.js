d94c8f572912a8b3c598ce2523921cfa180996c9
gxdxx <nkd0310@naver.com>
2021-03-15 01:07:05 +0900

    쿠키와 인증 - Secure & HttpOnly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -10,7 +10,9 @@ http.createServer(function(request, response) {
         'Set-Cookie':[
             'yummy_cookie=choco',                       //Session cookie
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}` //Permanent cookie
+            `Permanent=cookies; Max-Age=${60*60*24*30}`, //Permanent cookie
+            'Secure=Secure; Secure', //Https인 경우에만 웹브라우저가 웹서버에게 전송
+            'HttpOnly=HttpOnly; HttpOnly'   //웹브라우저와 웹서버가 통신할때만 쿠키를 발행(자바스크립트를 통해서 접근 못하게함)
         ]  //저장된 쿠키값을 Cookie헤더값을 통해 서버로 전송
     });
     response.end('Cookie!!');
