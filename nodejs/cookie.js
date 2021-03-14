04c2959cf93134225d6cc2f49acc8ceaafd37098
gxdxx <nkd0310@naver.com>
2021-03-14 22:08:28 +0900

    쿠키와 인증 - 쿠키의 읽기, cookie npm 설치

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,5 +1,11 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response) {
+    var cookies = {};
+    if(request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie); //쿠키값을 객체로 반환
+    }
     response.writeHead(200, {
         'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']  //저장된 쿠키값을 Cookie헤더값을 통해 서버로 전송
     });
