574345dec5c8b29cda320d8b80b176888ac5cab3
gxdxx <nkd0310@naver.com>
2021-03-15 00:09:56 +0900

    쿠키와 인증 - Session vs Permant

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -7,7 +7,11 @@ http.createServer(function(request, response) {
         cookies = cookie.parse(request.headers.cookie); //쿠키값을 객체로 반환
     }
     response.writeHead(200, {
-        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']  //저장된 쿠키값을 Cookie헤더값을 통해 서버로 전송
+        'Set-Cookie':[
+            'yummy_cookie=choco',                       //Session cookie
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}` //Permanent cookie
+        ]  //저장된 쿠키값을 Cookie헤더값을 통해 서버로 전송
     });
     response.end('Cookie!!');
 }).listen(3000);
