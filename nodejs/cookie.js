90fd044a9395edb4962a6ddf995a15c552377383
gxdxx <nkd0310@naver.com>
2021-03-15 01:16:35 +0900

    쿠키와 인증 - path & domain

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -8,11 +8,13 @@ http.createServer(function(request, response) {
     }
     response.writeHead(200, {
         'Set-Cookie':[
-            'yummy_cookie=choco',                       //Session cookie
+            'yummy_cookie=choco',                           //Session cookie
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`, //Permanent cookie
-            'Secure=Secure; Secure', //Https인 경우에만 웹브라우저가 웹서버에게 전송
-            'HttpOnly=HttpOnly; HttpOnly'   //웹브라우저와 웹서버가 통신할때만 쿠키를 발행(자바스크립트를 통해서 접근 못하게함)
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,    //Permanent cookie
+            'Secure=Secure; Secure',                        //Https인 경우에만 웹브라우저가 웹서버에게 전송
+            'HttpOnly=HttpOnly; HttpOnly',                  //웹브라우저와 웹서버가 통신할때만 쿠키를 발행(자바스크립트를 통해서 접근 못하게함)
+            'Path=Path; Path=/cookie',                      //특정 디렉토리에서만 쿠키가 활성화 되도록 함
+            'Domain=Domain; Domain=o2.org'                  //o2.org앞의 어떠한 서브 도메인 안에서도 나타남
         ]  //저장된 쿠키값을 Cookie헤더값을 통해 서버로 전송
     });
     response.end('Cookie!!');
