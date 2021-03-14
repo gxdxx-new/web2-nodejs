23a48d8973531210ddbbc9e8e85d098062a88d39
gxdxx <nkd0310@naver.com>
2021-03-15 02:49:16 +0900

    쿠키와 인증 - 인증구현 - 로그인 쿠키 생성

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ router.get('/', function(request, response) { //routing
     var list = template.list(request.list);
     var html = template.HTML(title, list,
         `
-        <form action="login_process" method="post">
+        <form action="/login/login_process" method="post">
             <p><input type="text" name="email" placeholder="email"></p>
             <p><input type="password" name="password" placeholder="password"></p>
             <p><input type="submit"></p>
@@ -18,6 +18,18 @@ router.get('/', function(request, response) { //routing
         `<a href="/topic/create">create</a>` ///create로 이동, home에서는 update 버튼 안나오게
     );
     response.send(html);
+});
+
+router.post('/login_process', function(request, response, next) { //topic.create에서 post방식으로 전송됨
+    var post = request.body;
+    if(post.email === 'nkd0310@naver.com' && post.password === '000000') {
+        response.cookie('email', `${post.email}`);
+        response.cookie('password', `${post.password}`);
+        response.cookie('nickname', 'gidon');
+        response.redirect('/');
+    } else {
+        response.send('Who?');
+    }
 });
 
 module.exports = router;
