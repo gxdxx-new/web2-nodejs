f32fd151ea67e7f6fdbffb2a20bfe58ee9828187
gxdxx <nkd0310@naver.com>
2021-03-16 19:51:16 +0900

    Express - Session & Auth - 인증구현 - 세션 미들웨어 설치

--- routes/login.js
+++ routes/login.js
@@ -5,14 +5,14 @@ var router = express.Router();
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 router.get('/', function(request, response) { //routing
-    var title = 'Login';
+    var title = 'WEB - Login';
     var list = template.list(request.list);
     var html = template.HTML(title, list,
         `
         <form action="/login/login_process" method="post">
             <p><input type="text" name="email" placeholder="email"></p>
             <p><input type="password" name="password" placeholder="password"></p>
-            <p><input type="submit"></p>
+            <p><input type="submit" value="login"></p>
         </form>
         `,
         `<a href="/topic/create">create</a>`, ///create로 이동, home에서는 update 버튼 안나오게
@@ -24,9 +24,8 @@ router.get('/', function(request, response) { //routing
 router.post('/login_process', function(request, response) {
     var post = request.body;
     if(post.email === 'nkd0310@naver.com' && post.password === '000000') {
-        response.cookie('email', `${post.email}`);
-        response.cookie('password', `${post.password}`);
-        response.cookie('nickname', 'gidon');
+        request.session.is_logined = true;
+        request.session.nickname = 'gidon';
         response.redirect('/');
     } else {
         response.send('Who?');
