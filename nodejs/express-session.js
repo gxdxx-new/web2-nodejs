07282bff8671de36325788a1ca8019ec4b7e36ff
gxdxx <nkd0310@naver.com>
2021-03-16 17:07:40 +0900

    Express Session & Auth - session store

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,14 +1,17 @@
 var express = require('express');
-var parseurl = require('parseurl');
 var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var app = express();
 
-app.use(session({
-    secret: 'kASDSADSADASDWDQ@O!!@',
-    resave: false,  //false: 세션 데이터가 바뀌기 전 까지는 세션 저장소 값을 저장하지 않음
-    saveUninitialized: true //세션이 필요하기 전 까지는 세션을 구동시키지 않음
-}));
+app.use(
+    session({
+        secret: 'kASDSADSADASDWDQ@O!!@',
+        resave: false,  //false: 세션 데이터가 바뀌기 전 까지는 세션 저장소 값을 저장하지 않음
+        saveUninitialized: true, //세션이 필요하기 전 까지는 세션을 구동시키지 않음
+        store:new FileStore()
+    })
+);
 
 app.get('/', function(request, response, next) {
     if(request.session.num === undefined) {
@@ -22,3 +25,30 @@ app.get('/', function(request, response, next) {
 app.listen(3000, function() {
     console.log('3000!');
 })
+
+// var express = require('express');
+// var parseurl = require('parseurl');
+// var session = require('express-session');
+// var FileStore = require('session-file-store')(session);
+
+// var app = express();
+
+// app.use(session({
+//     secret: 'kASDSADSADASDWDQ@O!!@',
+//     resave: false,  //false: 세션 데이터가 바뀌기 전 까지는 세션 저장소 값을 저장하지 않음
+//     saveUninitialized: true, //세션이 필요하기 전 까지는 세션을 구동시키지 않음
+//     store:new FileStore()
+// }));
+
+// app.get('/', function(request, response, next) {
+//     if(request.session.num === undefined) {
+//         request.session.num = 1;
+//     } else {
+//         request.session.num++;
+//     }
+//     response.send(`Views : ${request.session.num}`);
+// })
+
+// app.listen(3000, function() {
+//     console.log('3000!');
+// })
